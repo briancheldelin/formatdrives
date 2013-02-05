6836249146d4902acf0e7159d58b9704c3c243db
Brian Cheldelin <cheldelin.brian@gmail.com>
2013-02-05 13:29:25 -0800

    Updated script to work with unknow partion numbers

--- formatdrives.sh
+++ formatdrives.sh
@@ -17,7 +17,48 @@ do
 	echo "- - -" > /sys/class/scsi_host/host2/scan
 	echo "- - -" > /sys/class/scsi_host/host3/scan
 	sleep 2
+
+	# Remove each partition on sdb
+	for v_partition in $(parted -s /dev/sdb print|awk '/^ / {print $1}')
+	do
+	   parted -s /dev/sdb rm ${v_partition}
+	done
+
+	# Remove each partition on sdc
+	for v_partition in $(parted -s /dev/sdc print|awk '/^ / {print $1}')
+	do
+	   parted -s /dev/sdc rm ${v_partition}
+	done
+
+	# Remove each partition on sdd
+	for v_partition in $(parted -s /dev/sdd print|awk '/^ / {print $1}')
+	do
+	   parted -s /dev/sdd rm ${v_partition}
+	done
+
+	# Remove each partition on sde
+	for v_partition in $(parted -s /dev/sde print|awk '/^ / {print $1}')
+	do
+	   parted -s /dev/sde rm ${v_partition}
+	done
+
+	# Find size of disk and Create single partion
+	v_disk=$(parted -s /dev/sdb print|awk '/^Disk/ {print $3}'|sed 's/[Mm][Bb]//')
+	parted -s /dev/sdb mkpart primary 0 ${v_disk}
 	
+	# Find size of disk and Create single partion
+	v_disk=$(parted -s /dev/sdc print|awk '/^Disk/ {print $3}'|sed 's/[Mm][Bb]//')
+	parted -s /dev/sdc mkpart primary 0 ${v_disk}
+	
+	# Find size of disk and Create single partion
+	v_disk=$(parted -s /dev/sdd print|awk '/^Disk/ {print $3}'|sed 's/[Mm][Bb]//')
+	parted -s /dev/sdd mkpart primary 0 ${v_disk}
+
+	# Find size of disk and Create single partion
+	v_disk=$(parted -s /dev/sde print|awk '/^Disk/ {print $3}'|sed 's/[Mm][Bb]//')
+	parted -s /dev/sde mkpart primary 0 ${v_disk}
+	
+	# Format the partition
 	mkntfs -L "Default" /dev/sdb1 &
 	mkntfs -L "Default" /dev/sdc1 &
 	mkntfs -L "Default" /dev/sdd1 &
