57b693ac484fde8311360a095472fd7ca0ad25b7
Brian Cheldelin <cheldelin.brian@gmail.com>
2013-02-05 16:25:53 -0800

    specified a file system type when creating partions

--- formatdrives.sh
+++ formatdrives.sh
@@ -17,7 +17,8 @@ do
 	echo "- - -" > /sys/class/scsi_host/host2/scan
 	echo "- - -" > /sys/class/scsi_host/host3/scan
 	sleep 2
-
+	
+	echo "Removing Partitions"
 	# Remove each partition on sdb
 	for v_partition in $(parted -s /dev/sdb print|awk '/^ / {print $1}')
 	do
@@ -41,22 +42,23 @@ do
 	do
 	   parted -s /dev/sde rm ${v_partition}
 	done
-
+	
+	echo "creating partitions"
 	# Find size of disk and Create single partion
 	v_disk=$(parted -s /dev/sdb print|awk '/^Disk/ {print $3}'|sed 's/[Mm][Bb]//')
-	parted -s /dev/sdb mkpart primary 0 ${v_disk}
+	parted -s /dev/sdb mkpart primary ntfs 0 ${v_disk}
 	
 	# Find size of disk and Create single partion
 	v_disk=$(parted -s /dev/sdc print|awk '/^Disk/ {print $3}'|sed 's/[Mm][Bb]//')
-	parted -s /dev/sdc mkpart primary 0 ${v_disk}
+	parted -s /dev/sdc mkpart primary ntfs 0 ${v_disk}
 	
 	# Find size of disk and Create single partion
 	v_disk=$(parted -s /dev/sdd print|awk '/^Disk/ {print $3}'|sed 's/[Mm][Bb]//')
-	parted -s /dev/sdd mkpart primary 0 ${v_disk}
+	parted -s /dev/sdd mkpart primary ntfs 0 ${v_disk}
 
 	# Find size of disk and Create single partion
 	v_disk=$(parted -s /dev/sde print|awk '/^Disk/ {print $3}'|sed 's/[Mm][Bb]//')
-	parted -s /dev/sde mkpart primary 0 ${v_disk}
+	parted -s /dev/sde mkpart primary ntfs 0 ${v_disk}
 	
 	# Format the partition
 	mkntfs -L "Default" /dev/sdb1 &
